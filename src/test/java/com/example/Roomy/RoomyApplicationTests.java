e75283350e31573ab90d8ef1db781291e37c0ca6
YyyyyyCH <dusckd4948@naver.com>
2024-12-19 15:28:23 +0900

    start spring insert only spring-web

---
+++ src/test/java/com/example/Roomy/RoomyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Roomy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
